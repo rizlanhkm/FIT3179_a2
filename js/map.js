86e993a7b3c8c7a624f1f5fb5cfb35545dd8c37d
rizlanhkm <rmoh0031@student.monash.edu>
2024-10-08 17:39:56 +0800

    added line chart to the dashboard

--- js/map.js
+++ js/map.js
@@ -2,3 +2,8 @@ var vg_1 = "js/map.vg.json";
 vegaEmbed("#map_chart", vg_1).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
+
+var vg_2 = "js/chart.vg.json";
+vegaEmbed("#line_chart", vg_2).then(function(result) {
+    // Access the Vega view instance as result.view
+}).catch(console.error);

121c91a94b80f48ca3373ec02b383054e5a74999
rizlanhkm <rmoh0031@student.monash.edu>
2024-10-14 00:45:39 +0800

    Update charts.js

--- js/charts.js
+++ js/charts.js
@@ -1,9 +1,11 @@
 var vg_1 = "js/map.vg.json";
-vegaEmbed("#map_chart", vg_1).then(function(result) {
-    // Access the Vega view instance as result.view
-}).catch(console.error);
+vegaEmbed("#map_chart", vg_1).then(function(result) {}).catch(console.error);
 
-var vg_2 = "js/chart.vg.json";
-vegaEmbed("#line_chart", vg_2).then(function(result) {
-    // Access the Vega view instance as result.view
-}).catch(console.error);
+var vg_2 = "js/line_chart.vg.json";
+vegaEmbed("#line_chart", vg_2).then(function(result) {}).catch(console.error);
+
+var vg_3 = "js/urban_rate.vg.json";
+vegaEmbed("#urban_chart", vg_3).then(function(result) {}).catch(console.error);
+
+var vg_4 = "js/malaysia_map.vg.json";
+vegaEmbed("#malaysia_map", vg_4).then(function(result) {}).catch(console.error);
